002139c7033aa59f3005159dd4f8c72869e9edb3
Nurbol Bektibay <nurbol.bektibay@intexsoft.kz>
2022-12-16 20:18:57 +0600

    The application reads JSON. Also, it can accept custom type (runtime) and process errors.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,38 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	valueJSON := fmt.Sprintf("%d mins", r)
 	quotedValueJSON := strconv.Quote(valueJSON)
 	return []byte(quotedValueJSON), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	mins, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(mins)
+	return nil
 }
